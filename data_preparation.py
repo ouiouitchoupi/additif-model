97764693286d75c00019fb0fc038dfa78f1d7e4d
Theo Channarond <156784867+ouiouitchoupi@users.noreply.github.com>
2024-01-30 19:41:43 +0100

    data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,40 +1,55 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 class DataPreparation:
-	def __init__(self, csv_path):
-		"""
-		Cette classe prend en entrée un chemin de fichier csv.
-		Elle split le jeu de donnée en 2 bases 
-		+ une train 75 %
-		+ une test 25 %
-		Ce 2 bases, la classe va les splits en 2 
+    def __init__(self, csv_path):
+        """
+        Cette classe prend en entrée un chemin de fichier csv.
+        Elle split le jeu de donnée en 2 bases 
+        + une train 75 %
+        + une test 25 %
+        Ce 2 bases, la classe va les splits en 2 
 
-		+ un vecteur x (qui contient les indexs temporels)
-		+ un vecteyr y (qui contient les valeurs à prédire)
-		En tout cette va extraire 4 arrays.
-		x_train
-		y_train
-		x_test
-		y_test
-		"""
-		self.dataset_df = pandas.read_csv(csv_path)
-		self.dataset_df["month"] = pandas.to_datetime(self.dataset_df["month"])
-		self.prepare_data()
+        + un vecteur x (qui contient les indexs temporels)
+        + un vecteur y (qui contient les valeurs à prédire)
+        En tout cette va extraire 4 arrays.
+        x_train
+        y_train
+        x_test
+        y_test
+        """
+        self.dataset_df = pd.read_csv(csv_path)
+        self.dataset_df["Years"] = pd.to_datetime(self.dataset_df["Years"])
+        self.dataset_df["month_name"] = self.dataset_df["Years"].dt.month_name()
+        print(self.dataset_df)
 
-	def prepare_data(self):
-		number_of_rows = len(self.dataset_df)
-		self.dataset_df["index_mesure"] = numpy.arange(0, number_of_rows, 1)
 
-		dataset_train_df = self.dataset_df.iloc[ : int(number_of_rows*0.75)]
-		dataset_test_df = self.dataset_df.iloc[int(number_of_rows*0.75): ]
+        self.prepare_data()
 
-		self.x_train = dataset_train_df[['index_mesure']].values
-		self.y_train = dataset_train_df[['passengers']].values
+    def prepare_data(self):
+        number_of_rows = len(self.dataset_df)
+        self.dataset_df["index_mesure"] = np.arange(0, number_of_rows, 1)
 
-		self.x_test = dataset_test_df[['index_mesure']].values
-		self.y_test = dataset_test_df[['passengers']].values
+        # One-hot encode the 'month_name' column
+        month_dummies = pd.get_dummies(self.dataset_df["month_name"], prefix="month")
+        self.dataset_df = pd.concat([self.dataset_df, month_dummies], axis=1)
+
+        dataset_train_df = self.dataset_df.iloc[:int(number_of_rows * 0.75)]
+        dataset_test_df = self.dataset_df.iloc[int(number_of_rows * 0.75):]
+
+        # Extract features and labels
+        self.x_train = dataset_train_df[['index_mesure'] + list(month_dummies.columns)].values
+        self.y_train = dataset_train_df[['Sales']].values
+
+        self.x_test = dataset_test_df[['index_mesure'] + list(month_dummies.columns)].values
+        self.y_test = dataset_test_df[['Sales']].values
+
+    def show_graph(self):
+        plt.figure(figsize=(15, 6))
+        plt.plot(self.dataset_df["Years"], self.dataset_df["Sales"], "o:")
+        plt.show()
+
 
 
 	def show_graph(self):
